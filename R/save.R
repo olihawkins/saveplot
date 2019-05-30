c2ff50c23618afe36a434a3cf59409a12c705dfb
Oliver Hawkins <oli@olihawkins.com>
2019-05-30 13:14:24 +0100

    0.1.1 - Increased default ppi in sve_png

--- R/save.R
+++ R/save.R
@@ -21,10 +21,10 @@ save_svg <- function(plot, filename, width, height) {
 #' @param filename Filename to save to a png.
 #' @param width Width of the image in inches.
 #' @param height Height of the image in inches.
-#' @param ppi Number of pixels per inch for the png.
+#' @param ppi Number of pixels per inch for the png. The deafult is 400.
 #' @export
 
-save_png <- function(plot, filename, width, height, ppi = 220) {
+save_png <- function(plot, filename, width, height, ppi = 400) {
 
         svgname <- file.path(
             tempdir(),
